67892c7f9601e3c699df9009ee4bc40f6dc1c097
Dorothy Muhonja <dorothymuhonja7@gmail.com>
2022-08-29 16:50:38 +0300

    commit tree 37afcb69ccc0eb500cfafb4effef9085e3b71a25

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Code your solutions in this file
+
+// for ([initialization]; [condition]; [iteration]) {
+//   [loop body]
+// }
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+function writeCards(names, eventName) {
+    let messages = []
+    for(let i = 0; i < names.length; i++) {
+     const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+
+     messages.push(message);
+        debugger
+    }
+
+    return messages;
+
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    let i = 0;
+    while(i < gifts.length) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        i++;
+    }
+    return gifts;
+    
+}
+
+wrapGifts(gifts);
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num --);
+    }
+    return num
+}
+countDown(10)
